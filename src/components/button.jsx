1fff61839c37028cbece34a4f9b027d9a9934654
Clement Osei Asante <clemzymillz@gmail.com>
2021-07-08 09:30:03 +0000

    added header component no styling yet

---
+++ src/components/button.jsx
@@ -0,0 +1,7 @@
+const Button = ({text,color}) => {
+    return (
+        <button className='btn' style={{backgroundColor:color}}>{text}</button>
+    )
+}
+
+export default Button;
---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import Button from './button'
+const Header = () =>{
+    return (
+        <header class='header'>
+            <h2>Welcome To Task Tracker UI </h2>
+            <Button text = 'add' color='black' />
+            </header>
+    )
+}
+export default Header;
